4969413a105debcb9df0c1ca9b05d032ea4f282c
Eric Tillberg <eric.tillberg@gmail.com>
2015-05-07 19:33:51 -0400

    Fixed a map issue

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,9 +12,12 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require foundation
 
 //= require_tree .
 //= require bootstrap
 
 //= require underscore
 //= require gmaps/google
+
+$(function(){ $(document).foundation(); });
